077edf97d4dda23707b65655b8f8a46beadd4d42
Tina <skielred@gmail.com>
2019-04-19 03:51:32 +0200

    Fix build script syntax

--- build_linux.sh
+++ build_linux.sh
@@ -2,10 +2,11 @@
 
 out_folder='bin'
 out_name='crescent'
-src_files='crescent' '__main__.py'
+src_files='crescent __main__.py'
 zip_name='crescent.zip'
 
 mkdir -p "$out_folder"
 python3 -m zipfile -c "$out_folder"'/'"$zip_name" $src_files
-cat '#!/usr/bin/env python3' > "$out_folder"'/'"$out_name"
+echo '#!/usr/bin/env python3' > "$out_folder"'/'"$out_name"
 cat "$out_folder"'/'"$zip_name" >> "$out_folder"'/'"$out_name"
+chmod +x "$out_folder"'/'"$out_name"
